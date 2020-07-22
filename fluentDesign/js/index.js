40d79e1b04aed6acb174f5dd11d1cef8f9d261dc
eddie605134 <eddie605134@gmail.com>
2020-07-22 14:55:08 +0800

    7/22-3
    new

---
+++ fluentDesign/js/index.js
@@ -0,0 +1,18 @@
+
+window.onload = function() {
+	lax.setup() // init
+
+	const updateLax = () => {
+		lax.update(window.scrollY)
+		window.requestAnimationFrame(updateLax)
+	}
+
+	window.requestAnimationFrame(updateLax)
+}
+
+window.onscroll = function(){
+    // console.log(document.documentElement.scrollTop);
+}
+
+ 
+ 
